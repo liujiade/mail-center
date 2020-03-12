64a6c61cb4c5014502d4bcd1e6a16e0ac4284336
liujiande <liujiande@zichan360.com>
2020-03-12 13:44:12 +0800

    初始化

---
+++ mail-consumer/src/test/java/com/bruce/mailConsumer/MailConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.mailConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mail-producer/src/test/java/com/bruce/mailproducer/MailProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.mailproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
